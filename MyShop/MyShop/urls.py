bef6a4f356a89b84d158d7399a8b8f3129169ee4
kamleshlovewanshi2000 <85796224+kamleshlovewanshi2000@users.noreply.github.com>
2023-03-04 23:08:30 +0530

    update

--- MyShop/MyShop/urls.py
+++ MyShop/MyShop/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import views
 urlpatterns = [
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
